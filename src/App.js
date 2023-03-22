5ac6509350adee4505edb26a5ceb7b56d4758a29
Ludmilla <ludmilla.wagner93@gmail.com>
2023-03-22 16:00:26 +0100

    week 287

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './Tarif';
 import Tarif from './Tarif';
 
 const tarifs =[
-  {name:"Безлимитный 300", price:"300 руб/мес", speed:"до 10 Мбит/сек", more:"Объем включенного графика не ограничен"},
-  {name:"Безлимитный 450", price:"450 руб/мес",  speed:"до 50 Мбит/сек", more:"Объем включенного графика не ограничен"},
-  {name:"Безлимитный 550", price:"550 руб/мес",  speed:"до 100 Мбит/сек", more:"Объем включенного графика не ограничен", isSelected:true},
-  {name:"Безлимитный 1000", price:"1000 руб/мес",  speed:"до 200 Мбит/сек", more:"Объем включенного графика не ограничен"}
+  {id: 1, name:"Безлимитный 300", price:"300 руб/мес", speed:"до 10 Мбит/сек", more:"Объем включенного графика не ограничен"},
+  {id: 2, name:"Безлимитный 450", price:"450 руб/мес",  speed:"до 50 Мбит/сек", more:"Объем включенного графика не ограничен"},
+  {id: 3, name:"Безлимитный 550", price:"550 руб/мес",  speed:"до 100 Мбит/сек", more:"Объем включенного графика не ограничен", isSelected:true},
+  {id: 4, name:"Безлимитный 1000", price:"1000 руб/мес",  speed:"до 200 Мбит/сек", more:"Объем включенного графика не ограничен"}
 ]
 
 class  App extends React.Component {
@@ -16,7 +16,7 @@ class  App extends React.Component {
     <div className="App">
       {
         tarifs.map((tarif)=>
-        <Tarif name={tarif.name} price={tarif.price} speed={tarif.speed} more={tarif.more} isSelected={tarif.isSelected}></Tarif>
+        <Tarif key={tarif.id} name={tarif.name} price={tarif.price} speed={tarif.speed} more={tarif.more} isSelected={tarif.isSelected}></Tarif>
         )
       }
       
--- src/Tarif.jsx
+++ src/Tarif.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import './Tarif.css';
+import './Tarif.scss';
 
 class Tarif extends React.Component{
 render(){
     const {name,price, speed,more,isSelected}=this.props;
         return(
-            <div className={'tarif-card' +(isSelected? "selected":"")}>
-                <div className='tarif-name'>{name}</div>
-                <div className='tarif-price'>{price}</div>
-                <div className='tarif-speed'>{speed}</div>
-                <div className='tarif-more'>{more}</div>
+            <div className={"tarif-card" +(isSelected? "selected":"")}>
+                <div className="tarif-name">{name}</div>
+                <div className="tarif-price">{price}</div>
+                <div className="tarif-speed">{speed}</div>
+                <div className="tarif-more">{more}</div>
             </div>
 
         )
